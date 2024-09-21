ef54b91e34468b2296619ea9fceaca5c9bd84790
Patrick Her <renfu.her@gmail.com>
2024-09-21 13:15:42 +0800

    chore: Update MySQL connection details
    The MySQL connection details in the `MYSQL.php` file have been updated to use the local host (`127.0.0.1`) and a new password (`Ks80Oh5gUj1YCOPBeDIK`). The database name has also been changed to `consulate`.

--- include/MYSQL.php
+++ include/MYSQL.php
@@ -1,10 +1,10 @@
 <?php
 
-define('DB_HOST','139.162.5.79');
+define('DB_HOST','127.0.0.1');
 
 define('DB_USERNAME','consulate'); // login user name
-define('DB_PASSWORD','ziZ659Unefoc1cuswuFL'); // login password
-define('DB_DATABASE','goodwayt_con'); // database name
+define('DB_PASSWORD','Ks80Oh5gUj1YCOPBeDIK'); // login password
+define('DB_DATABASE','consulate'); // database name
 /**
 define('DB_USERNAME','root'); // login user name
 define('DB_PASSWORD','f8621001'); // login password
